9cd30a010debd087d1291ddfec9c3ef5c783685a
Bonzorno1 <121493715+Bonzorno1@users.noreply.github.com>
2023-03-03 18:14:04 +0100

    1234

--- skrypty/dowbut.js
+++
@@ -1,9 +0,0 @@
-let toggle = document.querySelector('.toggle');
-		let menu = document.querySelector('.menu');
-        let mod = document.querySelector('.mod');
-        let last = document.querySelector('.last')
-		toggle.onclick =function(){
-			menu.classList.toggle('active');
-            mod.classList.add('active');
-            last.classList.add('active');
-		}
